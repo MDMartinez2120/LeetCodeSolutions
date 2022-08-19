015b3b447c6d62b14aabe6d48d81958de94b65d9
Matthew Martinez <matt.martinez2120@gmail.com>
2022-08-19 15:08:57 -0500

    solutions for problems.

--- js/main.js
+++ js/main.js
@@ -1,69 +1,113 @@
-// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// //You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//
+// let twoSum = function(nums, target) {
+//     const lookup = {};
+//
+//     for (let i = 0; i < nums.length; i++){
+//         const diff = target - nums[i];
+//         const diffIndex = nums.indexOf(diff);
+//
+//         if (diffIndex !== -1 && diffIndex !== i){
+//             return [i, diffIndex];
+//         }
+//     }
+// };
+//
+// // Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+// const nums1 = [];
+// const nums2 = [2];
+//
+// let findMedianSortedArrays = function(nums1, nums2) {
+//     const mergedArray = nums1.concat(nums2);
+//     mergedArray.sort(function (a, b) {
+//         return a - b
+//     });
+//     const even = mergedArray.length / 2,
+//         isOdd = mergedArray.length % 2;
+//
+//     if (isOdd > 0) {
+//         return mergedArray[Math.floor(mergedArray.length / 2)];
+//     } else {
+//         return (mergedArray[even] + mergedArray[even - 1]) / 2;
+//     }
+// };
+//
+// console.log(findMedianSortedArrays(nums1, nums2));
+//
+// // You are given an array prices where prices[i] is the price of a given stock on the ith day.
+// // You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+//
+// let maxProfit = function(prices) {
+//     let maximumProfit = 0;
+//         let minPrice = prices[0]
+//
+//     for (let sell = 1; sell < prices.length; sell++){
+//         let sellPrice = prices[sell]
+//         let profit = sellPrice - minPrice
+//
+//         maximumProfit = Math.max(maximumProfit, profit)
+//
+//         if (sellPrice < minPrice) minPrice = sellPrice;
+//     }
+//     return maximumProfit
+// };
+//
+// // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+//
+// let containsDuplicate = function(nums) {
+//     const result = nums.some(element => {
+//         if (nums.indexOf(element) !== nums.lastIndexOf(element)){
+//             return true;
+//         }
+//         return false;
+//     })
+//     return result;
+// };
 
-let twoSum = function(nums, target) {
-    const lookup = {};
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+// You must write an algorithm that runs in O(n) time and without using the division operation.
 
-    for (let i = 0; i < nums.length; i++){
-        const diff = target - nums[i];
-        const diffIndex = nums.indexOf(diff);
+let productExceptSelf = function(nums) {
+    function getMaxNumber(){
+        let i;
 
-        if (diffIndex !== -1 && diffIndex !== i){
-            return [i, diffIndex];
+        let max = nums[0];
+
+        for (let i = 0; i < nums.length; i++){
+            if (nums[i] > max){
+                max = nums[i];
+            }
         }
     }
+    return getMaxNumber();
 };
 
-// Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
-const nums1 = [];
-const nums2 = [2];
+function doubleChar(str) {
 
-let findMedianSortedArrays = function(nums1, nums2) {
-    const mergedArray = nums1.concat(nums2);
-    mergedArray.sort(function (a, b) {
-        return a - b
-    });
-    const even = mergedArray.length / 2,
-        isOdd = mergedArray.length % 2;
+    return str.split('').map(function(e){
+        return e + e;
+    }).join('')
 
-    if (isOdd > 0) {
-        return mergedArray[Math.floor(mergedArray.length / 2)];
-    } else {
-        return (mergedArray[even] + mergedArray[even - 1]) / 2;
-    }
-};
+}
 
-console.log(findMedianSortedArrays(nums1, nums2));
-
-// You are given an array prices where prices[i] is the price of a given stock on the ith day.
-// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
-
-let maxProfit = function(prices) {
-    let maximumProfit = 0;
-        let minPrice = prices[0]
-
-    for (let sell = 1; sell < prices.length; sell++){
-        let sellPrice = prices[sell]
-        let profit = sellPrice - minPrice
-
-        maximumProfit = Math.max(maximumProfit, profit)
-
-        if (sellPrice < minPrice) minPrice = sellPrice;
-    }
-    return maximumProfit
-};
-
-// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
-
-let containsDuplicate = function(nums) {
-    const result = nums.some(element => {
-        if (nums.indexOf(element) !== nums.lastIndexOf(element)){
-            return true;
+function isPythagoreanTriple(integers) {
+    integers = integers.map(e => e*e);
+    integers.sort((a,b) => a-b);
+    for(let i=integers.length-1; i>1; i--) {
+        let j = 0;
+        let k = i-1;
+        while(j<k) {
+            if(integers[j]+integers[k]===integers[i]) {
+                let a = Math.sqrt(integers[j]);
+                let b = Math.sqrt(integers[k]);
+                let c = Math.sqrt(integers[i])
+                return true;
+            } else {
+                return false;
+            }
         }
-        return false;
-    })
-    return result;
-};
-
-
-
+    }
+    return -1;
+}

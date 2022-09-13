5647d0bff3a6d17d8b9029b3fe94134c25594bef
Matthew Martinez <matt.martinez2120@gmail.com>
2022-09-13 10:35:05 -0500

    added more solutions.

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@
 // };
 //
 // console.log(findMedianSortedArrays(nums1, nums2));
-//
+///////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 // // You are given an array prices where prices[i] is the price of a given stock on the ith day.
 // // You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 //
@@ -52,7 +52,7 @@
 //     }
 //     return maximumProfit
 // };
-//
+///////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 // // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 //
 // let containsDuplicate = function(nums) {
@@ -64,105 +64,135 @@
 //     })
 //     return result;
 // };
-
+///////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 // Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
 // The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 // You must write an algorithm that runs in O(n) time and without using the division operation.
 
-let productExceptSelf = function(nums) {
-    function getMaxNumber(){
-        let i;
+// let productExceptSelf = function(nums) {
+//     function getMaxNumber(){
+//         let i;
+//
+//         let max = nums[0];
+//
+//         for (let i = 0; i < nums.length; i++){
+//             if (nums[i] > max){
+//                 max = nums[i];
+//             }
+//         }
+//     }
+//     return getMaxNumber();
+// };
+///////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// function doubleChar(str) {
+//
+//     return str.split('').map(function(e){
+//         return e + e;
+//     }).join('')
+//
+// }
+///////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// function isPythagoreanTriple(integers) {
+//     integers = integers.map(e => e*e);
+//     integers.sort((a,b) => a-b);
+//     for(let i=integers.length-1; i>1; i--) {
+//         let j = 0;
+//         let k = i-1;
+//         while(j<k) {
+//             if(integers[j]+integers[k]===integers[i]) {
+//                 let a = Math.sqrt(integers[j]);
+//                 let b = Math.sqrt(integers[k]);
+//                 let c = Math.sqrt(integers[i])
+//                 return true;
+//             } else {
+//                 return false;
+//             }
+//         }
+//     }
+//     return -1;
+// }
+///////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// function isDivisible(n, x, y) {
+//     if(n % x === 0 && n % y === 0){
+//         return true;
+//     }else {
+//         return false;
+//     }
+// }
+///////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// const countBy = (x, n) => new Array(n).fill(0).map((v, i) => x * (i + 1));
+//
+// function descendingOrder(num){
+//     return parseInt(num.toString().split("").sort().reverse().join(""));
+// }
+///////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// function friend(friends){
+//     let newArr = [];
+//     for (let i = 0; i < friends.length; i++) {
+//         if (friends[i].length === 4) {
+//             newArr.push(friends[i]);
+//         }
+//     }
+//     return newArr;
+// }
+///////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// function getMiddle(s) {
+//     const middle = Math.floor(s.length / 2);
+//     let result = s[middle];
+//     if(s.length % 2 === 0) {
+//         result = s[middle - 1] + result;
+//     }
+//     return result;
+// }
+///////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// function highAndLow(numbers){
+//     let numArr = numbers.split(" ");
+//     let highestNum = Math.max(...numArr);
+//     let lowestNum = Math.min(...numArr);
+//     return highestNum + " " + lowestNum;
+// }
+///////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// function findNeedle(haystack) {
+//     for (let i = 0; i < haystack.length; i++) {
+//         if (haystack[i] === "needle") {
+//             return "found the needle at position " + i;
+//         }
+//     }
+// }
+///////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// function firstNonConsecutive(arr) {
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[i - 1] + 1 !== arr[i]) return arr[i];
+//     }
+//     return null;
+// }
+///////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+function vaporCode(string) {
+    return string.toUpperCase().replace(/\s/g, "").split("").join("  ");
+}
 
-        let max = nums[0];
-
-        for (let i = 0; i < nums.length; i++){
-            if (nums[i] > max){
-                max = nums[i];
-            }
-        }
+console.log(vaporCode('Lets go to the movies'));
+///////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+function rowSumOddNumbers(n) {
+    return n * n * n;
+}
+///////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+function addLetters(...letters) {
+    if (letters.length === 0) {
+        return 'z';
     }
-    return getMaxNumber();
-};
-
-function doubleChar(str) {
-
-    return str.split('').map(function(e){
-        return e + e;
-    }).join('')
-
+    let sum = letters.reduce(
+        (acc, val) => {
+            let newAcc = acc + val.charCodeAt(0) - 96;
+            return newAcc > 26 ? newAcc - 26 : newAcc;
+        },
+        0
+    );
+    return String.fromCharCode(sum + 96);
+}
+///////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+function maxMultiple(divisor, bound){
+    return bound - bound % divisor;
 }
 
-function isPythagoreanTriple(integers) {
-    integers = integers.map(e => e*e);
-    integers.sort((a,b) => a-b);
-    for(let i=integers.length-1; i>1; i--) {
-        let j = 0;
-        let k = i-1;
-        while(j<k) {
-            if(integers[j]+integers[k]===integers[i]) {
-                let a = Math.sqrt(integers[j]);
-                let b = Math.sqrt(integers[k]);
-                let c = Math.sqrt(integers[i])
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
-    return -1;
-}
-
-function isDivisible(n, x, y) {
-    if(n % x === 0 && n % y === 0){
-        return true;
-    }else {
-        return false;
-    }
-}
-
-const countBy = (x, n) => new Array(n).fill(0).map((v, i) => x * (i + 1));
-
-function descendingOrder(num){
-    return parseInt(num.toString().split("").sort().reverse().join(""));
-}
-
-function friend(friends){
-    let newArr = [];
-    for (let i = 0; i < friends.length; i++) {
-        if (friends[i].length === 4) {
-            newArr.push(friends[i]);
-        }
-    }
-    return newArr;
-}
-
-function getMiddle(s) {
-    const middle = Math.floor(s.length / 2);
-    let result = s[middle];
-    if(s.length % 2 === 0) {
-        result = s[middle - 1] + result;
-    }
-    return result;
-}
-
-function highAndLow(numbers){
-    let numArr = numbers.split(" ");
-    let highestNum = Math.max(...numArr);
-    let lowestNum = Math.min(...numArr);
-    return highestNum + " " + lowestNum;
-}
-
-function findNeedle(haystack) {
-    for (let i = 0; i < haystack.length; i++) {
-        if (haystack[i] === "needle") {
-            return "found the needle at position " + i;
-        }
-    }
-}
-
-function firstNonConsecutive(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i - 1] + 1 !== arr[i]) return arr[i];
-    }
-    return null;
-}
+console.log(maxMultiple(2, 7), 6)
